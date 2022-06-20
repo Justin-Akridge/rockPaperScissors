a1dcab33bdcf4d2bd605a548ecf47215d2680182
Justin Akridge <akridjus@gmail.com>
2022-06-19 21:05:54 -0400

    new features

--- app.js
+++ app.js
@@ -1,19 +1,51 @@
 const choices = ['rock','paper','scissors']
 function game(){
-  ///play the game
-  //play5 rounds
-  //console based
+  playRound();
 }
-function playerRound(){
-  const playerSelection = playerChoice();
-  const computerSelection = computerChoice();
+function playRound(){
+  const playerSelection = playerChoice()
+  const computerSelection = computerChoice()
+  const winner = checkWinner(playerSelection,computerSelection);
+  console.log(winner);
 }
 
-function playerchoice(){
+
+function playerChoice(){
   let input = prompt("Type: rock, paper, or scissors")
+  while(input== null){
+    input= prompt("Type: rock, paper, or scissors")
+  }
   input = input.toLowerCase()
+  let check= validateInput(input);
+  while(check==false){
+    prompt('Error: Type: rock, paper, or scissors')
+    input = input.toLowerCase();
+    check = validateInput(input);
+  }
+
 }
 
 function computerChoice(){
   return choices[Math.floor(Math.random()*choices.length)]
 }
+
+function validateInput(choice){
+  return choices.includes(choice);
+}
+
+function checkWinner(choiceP, choiceC){
+  if(choiceP === choiceC){
+    return "tie game";
+  }
+  else if(
+    (choiceP === "rock" && choiceC ==="scissors")|| 
+  (choiceP === "paper" && choiceC === "rock") ||
+  (choiceP ==="scissors" && choiceC==="paper")
+  ){
+    return "Player win";
+  }
+  else{
+    return "Computer wins";
+  }
+}
+game();
